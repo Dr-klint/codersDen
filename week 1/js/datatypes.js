2da3ab9152db5eb0887d6a78c2c4c4eaf0ec2a77
Dr-klint <okechukwuclinton005@gmail.com>
2022-10-11 16:33:50 +0100

    week 2 task 1

--- week 1/js/datatypes.js
+++ week 1/js/datatypes.js
@@ -1,3 +1,4 @@
+//Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
 console.log(typeof hello);
 console.log(typeof awesome);
 console.log(typeof stay);
--- week 1/js/variable.js
+++ week 1/js/variable.js
@@ -1,3 +1,4 @@
+//Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
 const hello = "welcome to coder's den";
 const awesome = true;
 let stay;
